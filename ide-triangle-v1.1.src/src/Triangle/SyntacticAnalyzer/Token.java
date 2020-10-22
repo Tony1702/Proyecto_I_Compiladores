93a0beca10fb627f16b5b23d4e3f184e30ee96ec
Fabipercu <31943724+Fabipercu@users.noreply.github.com>
2020-10-22 12:27:37 -0600

    Commit inicial

---
+++ ide-triangle-v1.1.src/src/Triangle/SyntacticAnalyzer/Token.java
@@ -0,0 +1,161 @@
+/*
+ * @(#)Token.java                        2.1 2003/10/07
+ *
+ * Copyright (C) 1999, 2003 D.A. Watt and D.F. Brown
+ * Dept. of Computing Science, University of Glasgow, Glasgow G12 8QQ Scotland
+ * and School of Computer and Math Sciences, The Robert Gordon University,
+ * St. Andrew Street, Aberdeen AB25 1HG, Scotland.
+ * All rights reserved.
+ *
+ * This software is provided free for educational use only. It may
+ * not be used for commercial purposes without the prior written permission
+ * of the authors.
+ */
+
+package Triangle.SyntacticAnalyzer;
+
+
+final class Token extends Object {
+
+  protected int kind;
+  protected String spelling;
+  protected SourcePosition position;
+
+  public Token(int kind, String spelling, SourcePosition position) {
+
+    if (kind == Token.IDENTIFIER) {
+      int currentKind = firstReservedWord;
+      boolean searching = true;
+
+      while (searching) {
+        int comparison = tokenTable[currentKind].compareTo(spelling);
+        if (comparison == 0) {
+          this.kind = currentKind;
+          searching = false;
+        } else if (comparison > 0 || currentKind == lastReservedWord) {
+          this.kind = Token.IDENTIFIER;
+          searching = false;
+        } else {
+          currentKind ++;
+        }
+      }
+    } else
+      this.kind = kind;
+
+    this.spelling = spelling;
+    this.position = position;
+
+  }
+
+  public static String spell (int kind) {
+    return tokenTable[kind];
+  }
+
+  public String toString() {
+    return "Kind=" + kind + ", spelling=" + spelling +
+      ", position=" + position;
+  }
+
+  // Token classes...
+
+  public static final int
+
+    // literals, identifiers, operators...
+    INTLITERAL	= 0,
+    CHARLITERAL	= 1,
+    IDENTIFIER	= 2,
+    OPERATOR	= 3,
+
+    // reserved words - must be in alphabetical order...
+    ARRAY		= 4,
+    BEGIN		= 5,
+    CASES               = 6,
+    CASE                = 7,      
+    CASE_RANGE          = 8,
+    CASE_LITERALS       = 9,      
+    CASE_LITERAL        = 10,
+    CONST		= 11,
+    DO			= 12,
+    ELSECASE		= 13,
+    ELSE		= 14,
+    END			= 15,
+    FUNC		= 16,
+    IF			= 17,
+    IN			= 18,
+    LET			= 19,
+    OF			= 20,
+    PROC		= 21,
+    RECORD		= 22,
+    THEN		= 23,
+    TYPE		= 24,
+    VAR			= 25,
+    WHILE		= 26,
+
+    // punctuation...
+    DOT			= 27,
+    COLON		= 28,
+    SEMICOLON           = 29,
+    COMMA		= 30,
+    BECOMES		= 31,
+    IS			= 32,
+
+    // brackets...
+    LPAREN		= 33,
+    RPAREN		= 34,
+    LBRACKET            = 35,
+    RBRACKET            = 36,
+    LCURLY		= 37,
+    RCURLY		= 38,
+
+    // special tokens...
+    EOT			= 39,
+    ERROR		= 40;
+
+  private static String[] tokenTable = new String[] {
+    "<int>",
+    "<char>",
+    "<identifier>",
+    "<operator>",
+    "array",
+    "begin",
+    "cases",
+    "case",
+    "case-range",
+    "case-literals",
+    "case-literal",
+    "const",
+    "do",
+    "elsecase",
+    "else",
+    "end",
+    "func",
+    "if",
+    "in",
+    "let",
+    "of",
+    "proc",
+    "record",
+    "then",
+    "type",
+    "var",
+    "while",
+    ".",
+    ":",
+    ";",
+    ",",
+    ":=",
+    "~",
+    "(",
+    ")",
+    "[",
+    "]",
+    "{",
+    "}",
+    "",
+    "<error>"
+  };
+
+  private final static int	firstReservedWord = Token.ARRAY,
+  				lastReservedWord  = Token.WHILE;
+
+}
